b8bf814781508cd61112f26ed1edf3c864904a19
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-09 23:42:08 +0800

    Clean up App.js for other components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
-import Button from "./components/Button";
-
 function App() {
-  return (
-    <div>
-      <div>
-        <Button success outline rounded className="mb-5">
-          <GoBell />
-          Save
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCloudDownload />
-          Submit
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          Click
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Check
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Delete
-        </Button>
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
