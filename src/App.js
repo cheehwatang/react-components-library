25a46a0e6aa1a387d1b0e71522cb4bf8a9072a11
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-09 23:27:57 +0800

    Adds functionality for button component to take in additional tailwind css className

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <div>
-        <Button success outline rounded>
+        <Button success outline rounded className="mb-5">
           <GoBell />
           Save
         </Button>
--- src/components/Button.js
+++ src/components/Button.js
@@ -11,30 +11,34 @@ function Button({
   rounded,
   ...rest
 }) {
-  const classes = className("flex items-center px-3 py-1.5 border", {
-    "border-sky-500": primary,
-    "border-gray-900": secondary,
-    "border-green-500": success,
-    "border-yellow-400": warning,
-    "border-red-500": danger,
+  const classes = className(
+    rest.className,
+    "flex items-center px-3 py-1.5 border",
+    {
+      "border-sky-500": primary,
+      "border-gray-900": secondary,
+      "border-green-500": success,
+      "border-yellow-400": warning,
+      "border-red-500": danger,
 
-    "bg-sky-500": primary && !outline,
-    "bg-gray-900": secondary && !outline,
-    "bg-green-500": success && !outline,
-    "bg-yellow-400": warning && !outline,
-    "bg-red-500": danger && !outline,
-    "bg-white": outline,
+      "bg-sky-500": primary && !outline,
+      "bg-gray-900": secondary && !outline,
+      "bg-green-500": success && !outline,
+      "bg-yellow-400": warning && !outline,
+      "bg-red-500": danger && !outline,
+      "bg-white": outline,
 
-    "text-white":
-      !outline && (primary || secondary || success || warning || danger),
-    "text-sky-500": outline && primary,
-    "text-gray-900": outline && secondary,
-    "text-green-500": outline && success,
-    "text-yellow-400": outline && warning,
-    "text-red-500": outline && danger,
+      "text-white":
+        !outline && (primary || secondary || success || warning || danger),
+      "text-sky-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
 
-    "rounded-full": rounded,
-  });
+      "rounded-full": rounded,
+    }
+  );
 
   return (
     <button {...rest} className={classes}>
