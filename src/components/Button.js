8d53941a83a13e98c203f7886bab153b99baa84a
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-09 23:13:37 +0800

    fix the bug in the classNames overriding, separate out based on conditions

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,20 +10,29 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = className("px-3 py-1.5 border", {
-    "border-blue-500 bg-blue-500 text-white": primary,
-    "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-white": success,
-    "border-yellow-400 bg-yellow-400 text-white": warning,
-    "border-red-500 bg-red-500 text-white": danger,
-    "rounded-full": rounded,
-    // the latter background color would override the former.
+  const classes = className("flex items-center px-3 py-1.5 border", {
+    "border-sky-500": primary,
+    "border-gray-900": secondary,
+    "border-green-500": success,
+    "border-yellow-400": warning,
+    "border-red-500": danger,
+
+    "bg-sky-500": primary && !outline,
+    "bg-gray-900": secondary && !outline,
+    "bg-green-500": success && !outline,
+    "bg-yellow-400": warning && !outline,
+    "bg-red-500": danger && !outline,
     "bg-white": outline,
-    "text-blue-500": outline && primary,
+
+    "text-white":
+      !outline && (primary || secondary || success || warning || danger),
+    "text-sky-500": outline && primary,
     "text-gray-900": outline && secondary,
     "text-green-500": outline && success,
     "text-yellow-400": outline && warning,
     "text-red-500": outline && danger,
+
+    "rounded-full": rounded,
   });
 
   return <button className={classes}>{children}</button>;
