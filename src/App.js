dd9f2c700d988942f07d92e0d2fc0947c6adede1
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-09 21:43:16 +0800

    Adds button.js to receive children to display in the button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Button from "./components/Button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Save</Button>
+      </div>
+      <div>
+        <Button>Submit</Button>
+      </div>
+      <div>
+        <Button>Click</Button>
+      </div>
+      <div>
+        <Button>Check</Button>
+      </div>
+      <div>
+        <Button>Delete</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
