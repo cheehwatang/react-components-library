dd7436dd5a34a9e1e7e4209d4153e6f2059c9162
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-10 15:13:56 +0800

    Adds style to the accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaAngleDown, FaAngleLeft } from "react-icons/fa";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -10,15 +11,27 @@ function Accordion({ items }) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = (
+      <span className="text-xl">
+        {isExpanded ? <FaAngleLeft /> : <FaAngleDown />}
+      </span>
+    );
+
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div
+          className="flex justify-between items-center p-3 bg-gray-50 border-b cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
