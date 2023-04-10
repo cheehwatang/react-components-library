48d6d9229333bc111236b7eb04c5be016f7da66c
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-10 14:12:37 +0800

    Adds example content to display in the accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,14 @@
-function Accordion() {
-  return <div>Accordion</div>;
+function Accordion({ items }) {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
