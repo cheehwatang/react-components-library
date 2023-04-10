09b77ce38e664d21c0ed13e549d6ce067200255a
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-10 16:54:10 +0800

    Adds functionality to toggle dropdown menu

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsOpen((currentIsOpen) => {
+      return !currentIsOpen;
+    });
+  };
+
   const renderedOptions = options.map((option) => {
     return <div key={option.value}>{option.label}</div>;
   });
 
   return (
     <div>
-      <div>Select...</div>
-      <div>{renderedOptions}</div>
+      <div onClick={handleToggleMenu}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
 }
