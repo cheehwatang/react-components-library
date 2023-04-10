20e72ee7ac71ed7e5c5be232367a93cc352fed11
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-10 15:41:57 +0800

    Moves the App content for accordion to AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import Accordion from "./components/Accordion";
-
 function App() {
-  const items = [
-    {
-      id: "1",
-      label: "Can I use JavaScript on a project?",
-      content:
-        "Yes, you can use JavaScript on a project. Yes, you can use JavaScript on a project. Yes, you can use JavaScript on a project.",
-    },
-    {
-      id: "2",
-      label: "Can I use React on a project?",
-      content:
-        "Yes, you can use React on a project. Yes, you can use React on a project. Yes, you can use React on a project.",
-    },
-    {
-      id: "3",
-      label: "Can I use TailwindCSS on a project?",
-      content:
-        "Yes, you can use TailwindCSS on a project. Yes, you can use TailwindCSS on a project. Yes, you can use TailwindCSS on a project.",
-    },
-  ];
-
-  return <Accordion items={items} />;
+  return <div>App</div>;
 }
 
 export default App;
