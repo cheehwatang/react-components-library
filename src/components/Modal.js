40d4b5b0fff8dd52dff4d9dad7db04574963b490
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-11 21:16:11 +0800

    Adds Modal to use Portal and close when clicked outside the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,20 @@
-function Modal() {
-  return <div>Modal</div>;
+import ReactDOM from "react-dom";
+
+function Modal({ children, actionBar, onClose }) {
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="absolute inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="absolute inset-40 p-10 bg-white flex flex-col">
+        <div className="flex flex-col justify-between grow">{children}</div>
+        <div className="flex justify-end">{actionBar}</div>
+      </div>
+    </div>,
+
+    document.querySelector(".modal-container")
+  );
 }
 
 export default Modal;
