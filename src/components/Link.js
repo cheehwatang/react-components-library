f117206d202df4cd5ca72b5cd1decbe6f1a29133
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-11 18:24:12 +0800

    Adds ctrl new tab functionality and styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,17 +1,23 @@
+import classNames from "classnames";
 import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
+  const classes = classNames("text-blue-500 underline cursor-pointer");
+
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
 
   return (
-    <a onClick={handleClick} href={to}>
+    <a className={classes} onClick={handleClick} href={to}>
       {children}
     </a>
   );
