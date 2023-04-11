e3620d5c066ffef2bc3c37bc14be99aba7de97e5
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-11 21:42:16 +0800

    Display data in the table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,26 @@
 function Table({ data }) {
-  return <div>Table</div>;
+  const renderedData = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td className={fruit.color}></td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedData}</tbody>
+    </table>
+  );
 }
 
 export default Table;
