1012d233ce29eff6c1e18be30c191d5dc5c1104e
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-12 11:26:03 +0800

    Adds sorting functionality for column with sortValue

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,17 +1,57 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
-  const { config } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  const { config, data } = props;
+
+  const handleClick = (label) => {
+    if (label !== sortBy) {
+      setSortBy(label);
+      setSortOrder("asc");
+    } else {
+      if (sortOrder === null) {
+        setSortOrder("asc");
+      } else if (sortOrder === "asc") {
+        setSortOrder("desc");
+      } else {
+        setSortOrder(null);
+      }
+    }
+  };
+
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
     }
 
-    return { ...column, header: () => <th>{column.label}</th> };
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>{column.label}</th>
+      ),
+    };
   });
 
-  return <Table {...props} config={updatedConfig} />;
+  return <Table {...props} config={updatedConfig} data={sortedData} />;
 }
 
 export default SortableTable;
