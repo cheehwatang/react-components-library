42b52608dd936d382103791354f6829ceb1baf40
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-12 12:10:04 +0800

    Refactor sorting function into use-sort hook

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,43 +1,14 @@
-import { useState } from "react";
 import Table from "./Table";
+import useSort from "../hooks/use-sort";
 import { FaCaretUp, FaCaretDown } from "react-icons/fa";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
   const { config, data } = props;
 
-  const handleClick = (label) => {
-    if (label !== sortBy) {
-      setSortBy(label);
-      setSortOrder("asc");
-    } else {
-      if (sortOrder === null) {
-        setSortOrder("asc");
-      } else if (sortOrder === "asc") {
-        setSortOrder("desc");
-      } else {
-        setSortOrder(null);
-      }
-    }
-  };
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
+  const { sortOrder, sortBy, sortedData, setSortColumn } = useSort({
+    config,
+    data,
+  });
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -49,7 +20,7 @@ function SortableTable(props) {
       header: () => (
         <th
           className="cursor-pointer"
-          onClick={() => handleClick(column.label)}
+          onClick={() => setSortColumn(column.label)}
         >
           <div className="flex items-center">
             {getIcons(column.label, sortBy, sortOrder)}
